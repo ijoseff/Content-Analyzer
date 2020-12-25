0190ba4538ffc8e28856abbdacb9eb1b42294802
Joseff Tan <66125884+ijoseff@users.noreply.github.com>
2020-12-25 23:21:34 +0800

    Update TP.py

--- TP.py
+++ TP.py
@@ -11,4 +11,10 @@ def app():
     st.image(image, caption = ' ', use_column_width = True)
 
     st.title("Topic Modeling")
-    st.header('This searches group of words (i.e topic) from the contents.')
+    st.header('Searches group of words (i.e topic) from the contents.')
+
+    # Collects user input features into dataframe
+    uploaded_file = st.file_uploader("Upload your input CSV file", type=["csv"])
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        contents = df['Content']
